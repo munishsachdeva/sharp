44fd35163459604a3916bfa82a5b5bd591fe7ed6
Munish <45123618+munishsachdeva@users.noreply.github.com>
2023-07-14 11:49:24 +0530

    GetElementsbyClassname

--- app.js
+++ app.js
@@ -13,3 +13,11 @@ header.style.borderBottom = 'solid 3px #000';
 let header2 = document.getElementsByClassName('title');
 header2[0].style.fontWeight = 'bold';
 header2[0].style.color = 'green';
+
+let items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = 'green';
+
+for(let i=0; i<items.length; i++){
+    items[i].style.fontWeight = 'bold';
+}
+
