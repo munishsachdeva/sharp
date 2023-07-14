ba033b106109eef51349511e955814164dac72c5
Munish <45123618+munishsachdeva@users.noreply.github.com>
2023-07-14 11:08:58 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('i learnt js');
+console.log(document.title);
+document.title = 'Munish';
+console.log(document.title);
+console.log(document.head);
+console.log(document.body);
+console.log(document.forms);
+
+let header = document.getElementById('main-header');
+
+header.style.borderBottom = 'solid 3px #000';
+
+// Now make ADD ITEM bold and chage the font color to greeen
+let header2 = document.getElementsByClassName('title');
+header2[0].style.fontWeight = 'bold';
+header2[0].style.color = 'green';
