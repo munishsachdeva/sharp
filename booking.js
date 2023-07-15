5e7743e61769c102521b476885b9aa8bce6e217a
Munish <45123618+munishsachdeva@users.noreply.github.com>
2023-07-15 22:15:23 +0530

    Delete data from Local Storage
    Add a Delete Button - I want to be able to delete a few of them from the local storage as well as the UI because they are not interested in our product maybe.

--- booking.js
+++ booking.js
@@ -1,41 +1,61 @@
 // USER FORM SCRIPT
-  
-  // Put DOM elements into variables
-  const myForm = document.querySelector('#my-form');
-  const nameInput = document.querySelector('#name');
-  const emailInput = document.querySelector('#email');
-  const msg = document.querySelector('.msg');
-  const userList = document.querySelector('#users');
-  
-  // Listen for form submit
-  myForm.addEventListener('submit', onSubmit);
-  
-  function onSubmit(e) {
+
+// Put DOM elements into variables
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+// Listen for form submit
+myForm.addEventListener('submit', onSubmit);
+
+// Function to delete a user
+function deleteUser(key, li) {
+    // Remove the user from the local storage
+    localStorage.removeItem(key);
+
+    // Remove the corresponding list item from the UI
+    li.remove();
+}
+
+
+function onSubmit(e) {
     e.preventDefault();
-  
+
     if (nameInput.value === '' || emailInput.value === '') {
-      msg.classList.add('error');
-      msg.innerHTML = 'Please enter all fields';
-      setTimeout(() => msg.remove(), 3000);
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+        setTimeout(() => msg.remove(), 3000);
     } else {
-      const user = {
-        name: nameInput.value,
-        email: emailInput.value
-      };
-  
-      // Generate a unique key for the user
-      const key = `user_${Date.now()}`;
-  
-      // Store the user object in the local storage using the generated key
-      localStorage.setItem(key, JSON.stringify(user));
-  
-      // Create new list item with user
-      const li = document.createElement('li');
-      li.appendChild(document.createTextNode(`${user.name}: ${user.email}`));
-      userList.appendChild(li);
-  
-      nameInput.value = '';
-      emailInput.value = '';
+        const user = {
+            name: nameInput.value,
+            email: emailInput.value
+        };
+
+        // Generate a unique key for the user
+        const key = `user_${Date.now()}`;
+
+        // Store the user object in the local storage using the generated key
+        localStorage.setItem(key, JSON.stringify(user));
+
+        // Create new list item with user
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${user.name}: ${user.email}`));
+        
+        // Create a delete button
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.addEventListener('click', () => deleteUser(key, li));
+
+        // Append the delete button to the list item
+        li.appendChild(deleteButton);
+
+        // Append the list item to the user list in the HTML
+        userList.appendChild(li);
+
+        nameInput.value = '';
+        emailInput.value = '';
     }
-  }
-  
+}
+
